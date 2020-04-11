31374e10ebcae8209113a544a3060f0374d8bd0c
mm-irvingleonard <irving.leonard@modmed.com>
2020-04-10 20:30:09 -0400

    fix(exemod): debugging

--- _modules/snmp.py
+++ _modules/snmp.py
@@ -39,9 +39,9 @@ def add_user(username, authpass, privpass, service_name = 'snmpd' read_only = Tr
 	- privpass: snmpv3 privacy password
 
 	'''
-	service_running = __salt__['service.status'](service_name)
-	if service_running:
-		__salt__['service.stop'](service_name)
+# 	service_running = __salt__['service.status'](service_name)
+# 	if service_running:
+# 		__salt__['service.stop'](service_name)
 
 	parameters = []
 
@@ -55,8 +55,8 @@ def add_user(username, authpass, privpass, service_name = 'snmpd' read_only = Tr
 	LOGGER.debug('Adding user %s ', username)
 	command_str = __salt__['cmd.run']('net-snmp-create-v3-user ' + ' '.join(parameters))
 	
-	if service_running:
-		__salt__['service.start'](service_name)
+# 	if service_running:
+# 		__salt__['service.start'](service_name)
 
 def del_user(username, service_name = 'snmpd', snmpd_conf_path = '/etc/snmp/snmpd.conf', snmpd_conf_var_path = '/var/lib/net-snmp/snmpd.conf'):
 	'''Delete user
